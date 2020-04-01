9e16ba8e975ca4b64710df4222349221904b98e9
rMahori <ravinder.nitks@gmail.com>
2020-04-01 21:15:59 +0530

    Updated unit tests for SmsNotificationQueueData with Payload info

--- mail_khaifa/notification/tests/tests_serializers.py
+++ mail_khaifa/notification/tests/tests_serializers.py
@@ -10,7 +10,7 @@ class QueuableSmsNotificationDataTestCases(TestCase):
     def setUp(self):
         eid=uuid.uuid4()
         e_type=AddrEntity.AddrEntityType.TYPE_CUSTOMER
-        addr_entity=AddrEntity.create(eid, e_type)
+        addr_entity=AddrEntity.create(eid, e_type, phones=["+917829876765"])
         self.addr_entity=addr_entity
 
     def test_simple(self):
@@ -30,3 +30,11 @@ class QueuableSmsNotificationDataTestCases(TestCase):
         }
         data, error=QueuableSmsNotificationData.from_request(request_data)
         self.assertEqual(error, Error.NO_ERROR)
+        self.assertEqual(data.get_payload(), {
+            'from_code':request_data['from_code'],
+            'from_phone':request_data['from_phone'],
+            'to_phones':self.addr_entity.get_phones(),
+            'sms_type':request_data['sms_type'],
+            'sms_text':request_data['sms_text'],
+            'template_name':request_data['template_name']
+        })
